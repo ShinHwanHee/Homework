11def2866e5d45de1bcf2b6043aca2ec1b1667d1
ShinHwanHee <sih305053@gmail.com>
2022-04-16 16:18:25 +0900

    상속

---
+++ src/inheritance/Parent.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Parent {
+    public static void main(String[] args){
+        String kind;  //종류
+        String color; //색깔
+        int price; //가격
+        String model; //모델
+        String Fuel; //연료
+    }
+}
